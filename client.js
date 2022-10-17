4229356cbe14298f90bdbcc05bf86f6c3de329de
Lucas Barrena <lucasbarrena98@gmail.com>
2022-10-17 20:35:50 -0300

    +

--- client.js
+++ client.js
@@ -1,19 +1,30 @@
+const { readPeerSeed } = require('./util.js')
 const minimist = require('minimist')
 const DHT = require('@hyperswarm/dht')
 const TTY = require('./tty/index.js')
-const Keychain = require('keypear')
 const goodbye = require('graceful-goodbye')
 
+// node client.js <server public key>
+// node client.js <server public key> --seed <client seed file>
+
 const argv = minimist(process.argv.slice(2))
-if (!argv._[0]) throw new Error('Public key is required, i.e. hypershell <public key>')
-const seed = argv.seed ? Buffer.from(argv.seed, 'hex') : Keychain.seed()
-const publicKey = Buffer.from(argv._[0], 'hex')
+if (!argv._[0]) throw new Error('Public key is required, i.e. hypershell <server public key>')
+const seed = readPeerSeed(argv.seed)
+const serverPublicKey = Buffer.from(argv._[0], 'hex')
 
 const node = new DHT()
-goodbye(() => node.destroy())
+goodbye(() => node.destroy(), 2)
 
 const keyPair = DHT.keyPair(seed)
-const socket = node.connect(publicKey, { keyPair })
+console.log('Your peer public key is:', keyPair.publicKey.toString('hex'))
+
+const socket = node.connect(serverPublicKey, { keyPair })
 socket.setKeepAlive(5000)
+goodbye(() => socket.end(), 1)
 
 process.stdin.pipe(socket).pipe(process.stdout)
+
+socket.on('error', function (error) {
+  console.error(error)
+  process.exit()
+})
--- server.js
+++ server.js
@@ -1,25 +1,37 @@
+const { readAuthorizedKeys, readPeerSeed, parseFirewall } = require('./util.js')
 const minimist = require('minimist')
 const DHT = require('@hyperswarm/dht')
 const TTY = require('./tty/index.js')
-const Keychain = require('keypear')
 const goodbye = require('graceful-goodbye')
 
+// node server.js
+// node server.js --firewall <authorized keys file>
+// node server.js --firewall <authorized keys file> --seed <server seed key>
+// node server.js --firewall <authorized keys file> <server seed file>
+
 const argv = minimist(process.argv.slice(2))
-const seed = argv._[0] ? Buffer.from(argv._[0], 'hex') : Keychain.seed()
-const firewall = parseFirewall(argv.firewall)
+const seed = argv.seed ? Buffer.from(argv.seed, 'hex') : readPeerSeed(argv._[0])
+const firewall = parseFirewall(readAuthorizedKeys(argv.firewall))
 
 const node = new DHT()
-goodbye(() => node.destroy())
+goodbye(() => node.destroy(), 2)
 
 const server = node.createServer({ firewall })
+goodbye(() => server.close(), 1)
 
 server.on('connection', function (socket) {
   console.log('remote public key', socket.remotePublicKey.toString('hex'))
+
+  socket.setKeepAlive(5000)
 
   socket.on('open', () => console.log(Date.now(), '(server side) socket opened'))
   socket.on('close', () => console.log(Date.now(), '(server side) socket closed'))
   socket.on('end', () => console.log(Date.now(), '(server side) socket ended / wants to end'))
   // socket.on('end', () => socket.end())
+
+  socket.on('error', function (error) {
+    console.error(error.code, error)
+  })
 
   const tty = new TTY()
   tty.attach(socket)
@@ -32,22 +44,3 @@ server.listen(keyPair).then(() => {
   console.log('To connect to this ssh server, on another computer run')
   console.log('hypershell ' + keyPair.publicKey.toString('hex'))
 })
-
-function parseFirewall (publicKeys) {
-  // console.log('parseFirewall', typeof publicKeys, publicKeys)
-
-  if (!publicKeys || typeof publicKeys === 'boolean') return () => false
-
-  publicKeys = publicKeys.toString().split(',').map(v => Buffer.from(v, 'hex'))
-
-  return function (remotePublicKey, remoteHandshakePayload) {
-    for (const publicKey of publicKeys) {
-      if (remotePublicKey.equals(publicKey)) {
-        console.log('firewall allowed')
-        return false
-      }
-    }
-    console.log('firewall denied')
-    return true
-  }
-}
