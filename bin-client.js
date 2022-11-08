5fb29b4a3935ee38effa1c8c421c474964b679c6
Lucas Barrena <lucasbarrena98@gmail.com>
2022-11-08 01:40:40 -0300

    Update bin-client.js

--- bin-client.js
+++ bin-client.js
@@ -10,9 +10,11 @@ program
   .description('Connect to a P2P shell.')
   .argument('<server public key>', 'Public key of the server')
   // .argument('<server public key>', 'Public key of the server or absolute path to server public key')
+
   .option('-f <filename>', 'Filename of the client seed key.', path.join(SHELLDIR, 'peer'))
-  .option('--upload-source <source>', 'Upload a source file from client to target path of the server.') // + it should be one option or just another bin like hypershell-cp
-  .option('--upload-target <target>', 'Upload a source file from client to target path of the server.')
+  .option('--upload-source <source path>', 'Upload a source file from client to target path of the server.') // + it should be one option or just another bin like hypershell-cp
+  .option('--upload-target <absolute target path>', 'Upload a source file from client to target path of the server.')
+
   // .option('--key <hex or z32>', 'Inline key for the client.')
   // .option('--connect <server public key>', 'Specifies the filename of the server public key')
   .action(require('./actions/client.js'))
