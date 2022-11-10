1de98ae110187b39b7667abd67395985aceb5bc2
Lucas Barrena <lucasbarrena98@gmail.com>
2022-11-10 16:32:59 -0300

    cleanup

--- bin-copy.js
+++ bin-copy.js
@@ -15,12 +15,3 @@ program
   .action(require('./actions/copy.js'))
 
 program.parseAsync()
-
-// (upload)   hypershell-copy <source local path> <pub key or name>:<target remote path>
-// (download) hypershell-copy <remote public key or name>:<source remote path> <target local path>
-// (both)     hypershell-copy <remote public key or name>:<source local path> <remote public key or name or name>:<target remote path>
-
-// [examples]
-// (upload)   hypershell-copy ./app.js @machine3:/home/user/Desktop/app-backup.js
-// (download) hypershell-copy <pub key or name>:/home/user/Desktop/app.js ./app-received.js
-// (both)     hypershell-copy machine3:/home/user/Desktop/app-backup.js machine4:/home/user/Desktop/another-backup.js
