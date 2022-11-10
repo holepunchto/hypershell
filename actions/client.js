78682882affa71a1d640fc0a919322ac23adf273
Lucas <lucasbarrena98@gmail.com>
2022-11-11 02:42:03 +0800

    Add known peers (aliases: name -> public key) (#13)
    * add known peers (aliases: name -> public key)
    
    * change default firewall filename
    
    * improve clean parsing authorized and known peers
    
    * feedback
    
    * maf feedback

--- actions/client.js
+++ actions/client.js
@@ -5,12 +5,19 @@ const Protomux = require('protomux')
 const c = require('compact-encoding')
 const goodbye = require('graceful-goodbye')
 const m = require('../messages.js')
+const { SHELLDIR } = require('../constants.js')
 
 module.exports = async function (serverPublicKey, options = {}) {
   const keyfile = path.resolve(options.f)
 
   if (!fs.existsSync(keyfile)) errorAndExit(keyfile + ' not exists.')
 
+  for (const peer of readKnownPeers()) {
+    if (peer.name === serverPublicKey) {
+      serverPublicKey = peer.publicKey
+      break
+    }
+  }
   serverPublicKey = Buffer.from(serverPublicKey, 'hex')
 
   const seed = Buffer.from(fs.readFileSync(keyfile, 'utf8'), 'hex')
@@ -103,4 +110,30 @@ function parseVariadic (rawArgs) {
   const index = rawArgs.indexOf('--')
   const variadic = index === -1 ? null : rawArgs.splice(index + 1)
   return variadic || []
+}
+
+function readKnownPeers () {
+  const filename = path.join(SHELLDIR, 'known_peers')
+
+  if (!fs.existsSync(filename)) {
+    console.log('Notice: creating default known peers', filename)
+    fs.mkdirSync(path.dirname(filename), { recursive: true })
+    fs.writeFileSync(filename, '# <name> <public key>\n', { flag: 'wx' })
+  }
+
+  try {
+    return fs.readFileSync(filename, 'utf8')
+      .split('\n')
+      .map(line => {
+        line = line.replace(/\s+/g, ' ').trim()
+        line = line.replace(/#.*$/, '').trim()
+        const i = line.indexOf(' ')
+        if (i > -1) return [line.slice(0, i), line.slice(i + 1)]
+      })
+      .filter(m => m && m[0] && m[1])
+      .map(m => ({ name: m[0], publicKey: m[1] }))
+  } catch (error) {
+    if (error.code === 'ENOENT') return []
+    throw error
+  }
 }
--- actions/server.js
+++ actions/server.js
@@ -6,7 +6,6 @@ const goodbye = require('graceful-goodbye')
 const PTY = require('tt-native')
 const Protomux = require('protomux')
 const c = require('compact-encoding')
-const { SHELLDIR } = require('../constants.js')
 const m = require('../messages.js')
 
 const isWin = os.platform() === 'win32'
@@ -19,13 +18,7 @@ module.exports = async function (options = {}) {
 
   if (!fs.existsSync(keyfile)) errorAndExit(keyfile + ' not exists.')
 
-  if (!fs.existsSync(firewall)) {
-    console.log('Notice: creating default firewall', firewall)
-    fs.mkdirSync(path.dirname(firewall), { recursive: true })
-    fs.writeFileSync(firewall, '', { flag: 'wx' })
-  }
-
-  if (!fs.existsSync(keyfile)) errorAndExit(keyfile + ' not exists.')
+  readAuthorizedPeers(firewall)
 
   const seed = Buffer.from(fs.readFileSync(keyfile, 'utf8'), 'hex')
   const keyPair = DHT.keyPair(seed)
@@ -45,9 +38,7 @@ module.exports = async function (options = {}) {
   console.log()
 
   function onFirewall (remotePublicKey, remoteHandshakePayload) {
-    const publicKeys = readAuthorizedKeys(firewall)
-
-    for (const publicKey of publicKeys) {
+    for (const publicKey of readAuthorizedPeers(firewall)) {
       if (remotePublicKey.equals(publicKey)) {
         console.log('Firewall allowed:', remotePublicKey.toString('hex'))
         return false
@@ -138,13 +129,22 @@ function onresize (data, channel) {
   pty.resize(data.width, data.height)
 }
 
-function readAuthorizedKeys (firewall) {
+function readAuthorizedPeers (filename) {
+  if (!fs.existsSync(filename)) {
+    console.log('Notice: creating default firewall', filename)
+    fs.mkdirSync(path.dirname(filename), { recursive: true })
+    fs.writeFileSync(filename, '# <public key>\n', { flag: 'wx' })
+  }
+
   try {
-    return fs.readFileSync(firewall, 'utf8')
+    return fs.readFileSync(filename, 'utf8')
       .split('\n')
-      .map(line => line.match(/([a-zA-Z0-9]*)/i))
+      .map(line => {
+        line = line.replace(/\s+/g, ' ').trim()
+        line = line.replace(/#.*$/, '').trim()
+        return line
+      })
       .filter(m => m)
-      .map(m => m[1])
       .map(v => Buffer.from(v, 'hex'))
   } catch (error) {
     if (error.code === 'ENOENT') return []
