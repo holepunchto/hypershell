54becd0b38ea89bdbd38f37a18f8a12174c00f19
Lucas Barrena <lucasbarrena98@gmail.com>
2022-11-10 16:26:09 -0300

    remove file transfers from client

--- actions/client.js
+++ actions/client.js
@@ -6,7 +6,6 @@ const c = require('compact-encoding')
 const goodbye = require('graceful-goodbye')
 const m = require('../messages.js')
 const { SHELLDIR } = require('../constants.js')
-const tar = require('tar-fs')
 
 const EMPTY = Buffer.alloc(0)
 
@@ -56,76 +55,15 @@ module.exports = async function (serverPublicKey, options = {}) {
       { encoding: c.buffer, onmessage: onstdout }, // stdout
       { encoding: c.buffer, onmessage: onstderr }, // stderr
       { encoding: c.uint, onmessage: onexitcode }, // exit code
-      { encoding: m.resize }, // resize
-      { encoding: m.buffer }, // upload files
-      { encoding: m.buffer, onmessage: ondownload } // download files
+      { encoding: m.resize } // resize
     ],
     onclose () {
       socket.end()
-
-      if (!this.userData) return
-
-      const { download } = this.userData
-      if (download && download.extract) download.extract.destroy()
-
-      const { upload } = this.userData
-      if (upload && upload.pack) upload.pack.destroy()
     }
   })
 
   const spawn = parseVariadic(this.rawArgs)
   const [command = '', ...args] = spawn
-
-  if (options.uploadSource && options.uploadTarget) {
-    channel.open({
-      upload: { target: options.uploadTarget }
-    })
-
-    const source = path.resolve(options.uploadSource)
-    let st
-    try {
-      st = fs.lstatSync(source)
-    } catch (error) {
-      if (error.code === 'ENOENT') console.log(source + ': No such file or directory')
-      else console.error(error.message)
-
-      // channel.close()
-      socket.destroy()
-      return
-    }
-
-    const header = { isDirectory: st.isDirectory() }
-    channel.messages[5].send(Buffer.from(JSON.stringify(header)))
-
-    const pack = tar.pack(source)
-
-    pack.once('error', function (error) {
-      console.error(error.message)
-      channel.close()
-    })
-
-    pipeToMessage(pack, channel.messages[5])
-
-    this.userData = {
-      upload: { pack }
-    }
-
-    return
-  }
-
-  if (options.downloadSource && options.downloadTarget) {
-    const target = path.resolve(options.downloadTarget)
-
-    channel.open({
-      download: { source: options.downloadSource }
-    })
-
-    channel.userData = {
-      download: { extract: null, target }
-    }
-
-    return
-  }
 
   channel.open({
     spawn: {
@@ -166,50 +104,6 @@ module.exports = async function (serverPublicKey, options = {}) {
   })
 }
 
-function ondownload (data, channel) {
-  const { download } = channel.userData
-
-  if (!download.extract) {
-    const header = JSON.parse(data.toString())
-    const { error, isDirectory } = header
-
-    if (error) {
-      if (error.code === 'ENOENT') console.log('hypershell-server:', error.path + ': No such file or directory')
-      else console.error(error.message)
-
-      channel.close()
-      return
-    }
-
-    const targetDir = isDirectory ? download.target : path.dirname(download.target)
-
-    const extract = tar.extract(targetDir, {
-      readable: true,
-      writable: true,
-      map (header) {
-        if (!isDirectory) header.name = path.basename(download.target)
-        return header
-      }
-    })
-
-    extract.on('error', function (error) {
-      console.error(error)
-      channel.close()
-    })
-
-    extract.on('finish', function () {
-      channel.close()
-    })
-
-    download.extract = extract
-
-    return
-  }
-
-  if (data.length) download.extract.write(data)
-  else download.extract.end()
-}
-
 function errorAndExit (message) {
   console.error('Error:', message)
   process.exit(1)
@@ -245,14 +139,4 @@ function readKnownPeers () {
     if (error.code === 'ENOENT') return []
     throw error
   }
-}
-
-function pipeToMessage (stream, message) {
-  stream.on('data', function (chunk) {
-    message.send(chunk)
-  })
-
-  stream.once('end', function () {
-    message.send(EMPTY)
-  })
 }
