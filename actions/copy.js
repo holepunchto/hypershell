947fe17d6c544afe4b92feb813bf1dff38602182
Lucas Barrena <lucasbarrena98@gmail.com>
2022-11-12 16:32:50 -0300

    Update copy.js

--- actions/copy.js
+++ actions/copy.js
@@ -8,21 +8,22 @@ const os = require('os')
 const ClientSocket = require('../lib/client-socket.js')
 
 const EMPTY = Buffer.alloc(0)
+const publicKeyExpr = /^([a-fA-F0-9]{64}|[ybndrfg8ejkmcpqxot1uwisza345h769]{52}):/i
 
 module.exports = async function (sourcePath, targetPath, options = {}) {
-  const fileOperation = sourcePath[0] === '@' ? 'download' : 'upload'
+  const fileOperation = sourcePath[0] === '@' || publicKeyExpr.test(sourcePath) ? 'download' : 'upload'
   let serverPublicKey = null
 
   const keyfile = path.resolve(options.f)
 
   if (!fs.existsSync(keyfile)) errorAndExit(keyfile + ' not exists.')
 
-  if (sourcePath[0] === '@') {
+  if (sourcePath[0] === '@' || publicKeyExpr.test(sourcePath)) {
     [serverPublicKey, sourcePath] = parseRemotePath(sourcePath)
-    if (!sourcePath) errorAndExit('Invalid source path.')
-  } else if (targetPath[0] === '@') {
+    if (!serverPublicKey || !sourcePath) errorAndExit('Invalid source path.')
+  } else if (targetPath[0] === '@' || publicKeyExpr.test(targetPath)) {
     [serverPublicKey, targetPath] = parseRemotePath(targetPath)
-    if (!targetPath) errorAndExit('Invalid target path.')
+    if (!serverPublicKey || !targetPath) errorAndExit('Invalid target path.')
   } else {
     errorAndExit('Invalid source or target path.')
   }
@@ -157,9 +158,13 @@ function errorAndExit (message) {
 }
 
 function parseRemotePath (str) {
+  // str has to start with @ or a public key
+
   const i = str.indexOf(':')
   if (i === -1) return [null, null]
-  return [str.slice(1, i), str.slice(i + 1)] // [host, path]
+
+  const isName = str[0] === '@'
+  return [str.slice(isName ? 1 : 0, i), str.slice(i + 1)] // [host, path]
 }
 
 // Based on expand-home-dir
