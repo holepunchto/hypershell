b9e3f50732d44767a6eac3ff4d4f2ea976900d13
Lucas Barrena <lucasbarrena98@gmail.com>
2022-12-10 17:27:11 -0300

    using makeProtocol (mux.pair) for server

--- actions/server.js
+++ actions/server.js
@@ -74,157 +74,176 @@ function onConnection (socket) {
 
   const mux = new Protomux(socket)
 
-  const spawn = mux.createChannel({
-    protocol: 'hypershell',
-    id: null,
-    handshake: m.handshakeSpawn,
-    onopen (handshake) {
-      let pty
-      try {
-        pty = PTY.spawn(handshake.file || shellFile, handshake.args, {
-          cwd: os.homedir(),
-          env: process.env,
-          width: handshake.width,
-          height: handshake.height
-        })
-      } catch (error) {
-        spawn.messages[3].send(1)
-        spawn.messages[2].send(Buffer.from(error.toString() + '\n'))
-        spawn.close()
-        return
-      }
-
-      pty.on('data', function (data) {
-        spawn.messages[1].send(data)
-      })
-
-      pty.once('exit', function (code) {
-        spawn.messages[3].send(code)
-      })
-
-      pty.once('close', function () {
-        spawn.close()
-      })
-
-      this.userData = { pty }
-    },
-    messages: [
-      { encoding: c.buffer, onmessage: onstdin }, // stdin
-      { encoding: c.buffer }, // stdout
-      { encoding: c.buffer }, // stderr
-      { encoding: c.uint }, // exit code
-      { encoding: m.resize, onmessage: onresize } // resize
-    ],
-    onclose () {
-      if (!this.userData) return
-
-      const { pty } = this.userData
-      if (pty) {
+  makeProtocol(mux, { protocol: 'hypershell', id: null, _onlyPair: true }, function () {
+    const channel = mux.createChannel({
+      protocol: 'hypershell',
+      id: null,
+      handshake: m.handshakeSpawn,
+      onopen (handshake) {
+        let pty
         try {
-          pty.kill('SIGKILL')
-        } catch {} // ignore "Process has exited"
-      }
-    }
-  })
-
-  const upload = mux.createChannel({
-    protocol: 'hypershell-upload',
-    id: null,
-    handshake: m.handshakeUpload,
-    onopen (handshake) {
-      const { target, isDirectory } = handshake
-
-      const dir = isDirectory ? target : path.dirname(target)
-      const extract = tar.extract(dir, {
-        readable: true,
-        writable: true,
-        map (header) {
-          if (!isDirectory) header.name = path.basename(target)
-          return header
+          pty = PTY.spawn(handshake.file || shellFile, handshake.args, {
+            cwd: os.homedir(),
+            env: process.env,
+            width: handshake.width,
+            height: handshake.height
+          })
+        } catch (error) {
+          this.messages[3].send(1)
+          this.messages[2].send(Buffer.from(error.toString() + '\n'))
+          this.close()
+          return
         }
-      })
 
-      extract.once('error', function (error) {
-        upload.messages[1].send(error)
-        upload.close()
-      })
+        pty.on('data', (data) => {
+          this.messages[1].send(data)
+        })
 
-      extract.once('finish', () => upload.close())
+        pty.once('exit', (code) => {
+          this.messages[3].send(code)
+        })
 
-      upload.userData = { extract }
-    },
-    messages: [
-      null, // no header
-      { encoding: m.error }, // errors
-      { encoding: c.raw, onmessage: onupload } // data
-    ],
-    onclose () {
-      if (upload.userData) upload.userData.extract.destroy()
-    }
-  })
+        pty.once('close', () => {
+          this.close()
+        })
 
-  const download = mux.createChannel({
-    protocol: 'hypershell-download',
-    id: null,
-    handshake: m.handshakeDownload,
-    onopen (handshake) {
-      const { source } = handshake
+        this.userData = { pty }
+      },
+      messages: [
+        { encoding: c.buffer, onmessage: onstdin }, // stdin
+        { encoding: c.buffer }, // stdout
+        { encoding: c.buffer }, // stderr
+        { encoding: c.uint }, // exit code
+        { encoding: m.resize, onmessage: onresize } // resize
+      ],
+      onclose () {
+        if (!this.userData) return
 
-      try {
-        const st = fs.lstatSync(source)
-        download.messages[0].send({ isDirectory: st.isDirectory() })
-      } catch (error) {
-        download.messages[1].send(error)
-        download.close()
-        return
+        const { pty } = this.userData
+        if (pty) {
+          try {
+            pty.kill('SIGKILL')
+          } catch {} // ignore "Process has exited"
+        }
       }
+    })
 
-      const pack = tar.pack(source)
-      download.userData = { pack }
+    if (!channel) return
 
-      pack.once('error', function (error) {
-        download.messages[1].send(error)
-        download.close()
-      })
-
-      pack.on('data', (chunk) => download.messages[2].send(chunk))
-      pack.once('end', () => download.messages[2].send(EMPTY))
-    },
-    messages: [
-      { encoding: m.downloadHeader }, // header
-      { encoding: m.error }, // errors
-      { encoding: c.raw } // data
-    ],
-    onclose () {
-      if (download.userData) download.userData.pack.destroy()
-    }
+    channel.open({})
   })
 
-  const local = mux.createChannel({
-    protocol: 'hypershell-tunnel-local',
-    id: null,
-    handshake: c.json,
-    onopen (handshake) {
-      this.userData = { node, socket, handshake, streams: new Map() } // + try to not pass { node, socket, handshake }
-    },
-    messages: [
-      { encoding: c.json, onmessage: onstreamid },
-    ],
-    onclose () {
-      if (!this.userData) return
+  makeProtocol(mux, { protocol: 'hypershell-upload', id: null, _onlyPair: true }, function () {
+    const channel = mux.createChannel({
+      protocol: 'hypershell-upload',
+      id: null,
+      handshake: m.handshakeUpload,
+      onopen (handshake) {
+        const { target, isDirectory } = handshake
 
-      const { streams } = this.userData
+        const dir = isDirectory ? target : path.dirname(target)
+        const extract = tar.extract(dir, {
+          readable: true,
+          writable: true,
+          map (header) {
+            if (!isDirectory) header.name = path.basename(target)
+            return header
+          }
+        })
 
-      for (const [, stream] of streams) {
-        stream.destroy()
+        extract.once('error', (error) => {
+          this.messages[1].send(error)
+          this.close()
+        })
+
+        extract.once('finish', () => this.close())
+
+        this.userData = { extract }
+      },
+      messages: [
+        null, // no header
+        { encoding: m.error }, // errors
+        { encoding: c.raw, onmessage: onupload } // data
+      ],
+      onclose () {
+        if (this.userData) this.userData.extract.destroy()
       }
-    }
+    })
+
+    if (!channel) return
+
+    channel.open({})
   })
 
-  spawn.open({})
-  upload.open({})
-  download.open({})
-  local.open({})
+  makeProtocol(mux, { protocol: 'hypershell-download', id: null, _onlyPair: true }, function () {
+    const channel = mux.createChannel({
+      protocol: 'hypershell-download',
+      id: null,
+      handshake: m.handshakeDownload,
+      onopen (handshake) {
+        const { source } = handshake
+
+        try {
+          const st = fs.lstatSync(source)
+          this.messages[0].send({ isDirectory: st.isDirectory() })
+        } catch (error) {
+          this.messages[1].send(error)
+          this.close()
+          return
+        }
+
+        const pack = tar.pack(source)
+        this.userData = { pack }
+
+        pack.once('error', (error) => {
+          this.messages[1].send(error)
+          this.close()
+        })
+
+        pack.on('data', (chunk) => this.messages[2].send(chunk))
+        pack.once('end', () => this.messages[2].send(EMPTY))
+      },
+      messages: [
+        { encoding: m.downloadHeader }, // header
+        { encoding: m.error }, // errors
+        { encoding: c.raw } // data
+      ],
+      onclose () {
+        if (this.userData) this.userData.pack.destroy()
+      }
+    })
+
+    if (!channel) return
+
+    channel.open({})
+  })
+
+  makeProtocol(mux, { protocol: 'hypershell-tunnel-local', id: null, _onlyPair: true }, function () {
+    const channel = mux.createChannel({
+      protocol: 'hypershell-tunnel-local',
+      id: null,
+      handshake: c.json,
+      onopen (handshake) {
+        this.userData = { node, socket, handshake, streams: new Map() } // + try to not pass { node, socket, handshake }
+      },
+      messages: [
+        { encoding: c.json, onmessage: onstreamid },
+      ],
+      onclose () {
+        if (!this.userData) return
+
+        const { streams } = this.userData
+
+        for (const [, stream] of streams) {
+          stream.destroy()
+        }
+      }
+    })
+
+    if (!channel) return
+
+    channel.open({})
+  })
 }
 
 function onstdin (data, channel) {
@@ -342,4 +361,22 @@ function waitForSocketTermination (socket) {
       resolve()
     }
   })
+}
+
+function makeProtocol (mux, opts, cb) {
+  mux.pair(opts, callback)
+
+  if (!opts._onlyPair) {
+    mux.stream.opened.then(function (opened) {
+      if (opened) callback()
+    })
+  }
+
+  function callback () {
+    if (!mux.opened(opts)) cb()
+  }
+
+  return function () {
+    mux.unpair(opts)
+  }
 }
