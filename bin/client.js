30cbecd70d4dbf78620efd7857de96baa415ed56
Lucas Barrena <lucasbarrena98@gmail.com>
2023-03-24 23:58:10 -0300

    Allow multiple -L (local tunnels)

--- bin/client.js
+++ bin/client.js
@@ -16,7 +16,7 @@ program
   .description('Connect to a P2P shell.')
   .argument('<server public key or name>', 'Public key or name of the server')
   .option('-f <filename>', 'Filename of the client seed key.', path.join(SHELLDIR, 'peer'))
-  .option('-L <[address:]port:host:hostport>', 'Local port forwarding.')
+  .option('-L <[address:]port:host:hostport...>', 'Local port forwarding.')
   // .option('-R <[address:]port:host:hostport>', 'Remote port forwarding.')
   // .option('--key <hex or z32>', 'Inline key for the client.')
   // .option('--connect <server public key>', 'Specifies the filename of the server public key')
@@ -31,17 +31,22 @@ async function cmd (serverPublicKey, options = {}) {
     await keygen({ f: keyfile })
   }
 
-  const { node, socket } = ClientSocket({ keyfile, serverPublicKey, testnet: options.testnet })
-  const mux = new Protomux(socket)
-
   if (options.L) {
-    const tunnel = new LocalTunnelClient(options.L, { node, socket, mux })
-    tunnel.open()
+    for (const config of options.L) {
+      const { node, socket } = ClientSocket({ keyfile, serverPublicKey, testnet: options.testnet })
+      const mux = new Protomux(socket)
+
+      const tunnel = new LocalTunnelClient(config, { node, socket, mux })
+      tunnel.open()
+    }
     return
   } else if (options.R) {
     errorAndExit('-R not supported yet')
     return
   }
+
+  const { node, socket } = ClientSocket({ keyfile, serverPublicKey, testnet: options.testnet })
+  const mux = new Protomux(socket)
 
   const shell = new ShellClient(this.rawArgs, { node, socket, mux })
   shell.open()
